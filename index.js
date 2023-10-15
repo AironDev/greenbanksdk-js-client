78e5f1d899270ab6fbe979fe78b28a7281b00379
airondevt <airondev@gmail.com>
2023-10-15 20:53:53 +0100

    fixes

--- index.js
+++ index.js
@@ -82,8 +82,8 @@
 
         const response = await fetch(url, {
             method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, *cors, same-origin
-            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            // mode: "cors", // no-cors, *cors, same-origin
+            // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
             headers: {
                 "Content-Type": "application/json",
                 "Merchant-Apikey": payload.merchant_key,
@@ -195,8 +195,8 @@
 
         const response = await fetch(url, {
             method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, *cors, same-origin
-            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            // mode: "cors", // no-cors, *cors, same-origin
+            // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
             headers: {
                 "Content-Type": "application/json",
                 "Merchant-Apikey": data.merchant_key,
